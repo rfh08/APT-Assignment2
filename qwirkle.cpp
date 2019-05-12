559ed6caa24927cdbbcc658fa32f68b2c5f280a4
Hin97 <Jacklee5230@hotmail.com>
2019-05-12 17:07:41 +1000

    Edit Makefile, Implementing Qwirkle with testing

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -1,15 +1,24 @@
-
+﻿
 #include "LinkedList.h"
-
+#include "GameController.h"
 #include <iostream>
-
+#include <stdio.h>
 #define EXIT_SUCCESS    0
 
-int main(void) {
-   LinkedList* list = new LinkedList();
-   delete list;
+int main(int argc, char *argv[])
+{
+	if(argc==3)
+	{
+	//If command arguments more than 1, then we put the second argument as the file to stdin.
+	//Third argument will be the file that stdout result
+		std::cout << "testing " << argv[1] << "..." << std::endl;		//Print testing message
+		freopen(argv[1], "r", stdin);						   		 	//Stdin from input file
+		freopen(argv[2], "w", stdout);									//Stdout from output file
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
-
-   return EXIT_SUCCESS;
+	}
+	Game game;
+	game.Start();
+	getchar();
+	getchar();
+	return EXIT_SUCCESS;
 }
