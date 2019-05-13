240428ab9a084464674b25eb9abae20640b513b1
Hin97 <Jacklee5230@hotmail.com>
2019-05-13 12:27:52 +1000

    Upload Player

---
+++ Player.h
@@ -0,0 +1,18 @@
+﻿
+#ifndef ASSIGN2_PLAYER_H
+#define ASSIGN2_PLAYER_H
+#include <string>
+#include "LinkedList.h"
+
+class Player
+{
+public:
+	Player();
+	~Player();
+	//Player name
+	std::string name;
+	//Player score
+	int score;
+};
+
+#endif
